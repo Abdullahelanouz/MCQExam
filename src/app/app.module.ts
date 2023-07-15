13ad11f4020045863b001df968a42d26476307fa
AbdallaEbrahimElanoz <anaabdoa319@gmail.com>
2023-07-15 19:00:52 +0300

    create components and Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NewExamComponent } from './doctor/new-exam/new-exam.component';
+import { StudentsComponent } from './doctor/students/students.component';
+import { SubjectsComponent } from './doctor/subjects/subjects.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { ExamComponent } from './student/exam/exam.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NewExamComponent,
+    StudentsComponent,
+    SubjectsComponent,
+    NavbarComponent,
+    ExamComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [
+    LoginComponent,
+    RegisterComponent,
+    NewExamComponent,
+    StudentsComponent,
+    SubjectsComponent,
+    NavbarComponent,
+    ExamComponent
+  ]
 })
 export class AppModule { }
