262dddd76ac835ea3cfd75819f5a55919f44c96c
AbdallaEbrahimElanoz <anaabdoa319@gmail.com>
2023-07-18 16:53:26 +0300

    Create New Exam

--- src/app/doctor/doctor.module.ts
+++ src/app/doctor/doctor.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { SharedModule } from '../shared/shared.module';
+import { NewExamComponent } from './new-exam/new-exam.component';
+import { StudentsComponent } from './students/students.component';
+import { SubjectsComponent } from './subjects/subjects.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NewExamComponent,
+    StudentsComponent,
+    SubjectsComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
   ]
 })
 export class DoctorModule { }
